652ac458c57da8551d1c371013d0611e35e7e872
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-02-14 10:40:23 +0000

    Task0 and 1 updated

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,21 +14,21 @@ class TestAccessNestedMap(TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_output):
         """ method to test that the method returns what it is supposed to. """
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
 
     @parameterized.expand([
-        ({}, ("a",), 'a'),
-        ({"a": 1}, ("a", "b"), 'b')
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path, unexp_output):
         """ KeyError Test method """
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
-        self.assertEqual(unexp_output, error.exception)
+        self.assertEqual(error.exception, unexp_output)
 
 
 class TestGetJson(TestCase):
